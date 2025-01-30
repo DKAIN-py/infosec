4b0bef342dbe044188457288523576075ccc7f63
DKAIN-py <divyanshu06kain@gmail.com>
2025-01-30 13:05:35 +0530

    final changes

--- myApp.js
+++ myApp.js
@@ -1,61 +1,50 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Use Helmet for security headers
 app.use(helmet());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const api = require('./server.js');
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
+
+// Disable Strict Transport Security (HSTS) - only if necessary
 app.disable('strict-transport-security');
+
+// Import and use the API routes
+const api = require('./server.js');
 app.use('/_api', api);
+
+// Serve the main HTML file
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start the server
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server for learning infosec started on port ${port}`);
 });
+
+// Export the app for testing or other modules
 module.exports = app;
